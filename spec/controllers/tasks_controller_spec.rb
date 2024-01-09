08b6be125b53a9315e1add155ef528d8ea63c3e3
h8rry <harrychenca@gmail.com>
2024-01-09 13:45:38 +0800

    To pass all tests

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
-require 'factory_bot_rails'
 
 RSpec.describe TasksController, type: :controller do
   render_views
 
-  let!(:task1) { create(:task) }
-  let!(:task2) { create(:task) }
-
   describe 'GET /tasks' do
-    it "renders all tasks in JSON" do
+    it 'renders all tasks in JSON' do
+      task1 = Task.create(content: 'Task #1')
+      task2 = Task.create(content: 'Task #2')
+
       get :index
 
       expected_response = {
@@ -17,15 +16,12 @@ RSpec.describe TasksController, type: :controller do
             id: task1.id,
             content: task1.content,
             completed: task1.completed,
-            created_at: task1.created_at,
-            updated_at: task1.updated_at 
-          },
-          {
+            created_at: task1.created_at
+          }, {
             id: task2.id,
             content: task2.content,
             completed: task2.completed,
-            created_at: task2.created_at,
-            updated_at: task2.updated_at 
+            created_at: task2.created_at
           }
         ]
       }
@@ -36,43 +32,46 @@ RSpec.describe TasksController, type: :controller do
 
   describe 'POST /tasks' do
     it 'renders newly created task in JSON' do
-      post :create, params: { task: { content: 'New Task' } }, format: :json
-
-      expected_response = {
+      post :create, params: {
         task: {
-          id: Task.last.id,
-          content: 'New Task',
-          completed: false,
-          created_at: Task.last.created_at,
-          updated_at: Task.last.updated_at
+          content: 'New Task'
         }
       }
 
-      expect(response.body).to eq(expected_response.to_json)
+      expect(Task.count).to eq(1)
+
+      expect(response.body).to eq({
+        task: {
+          id: Task.first.id,
+          content: 'New Task',
+          completed: false,
+          created_at: Task.first.created_at
+        }
+      }.to_json)
     end
   end
 
   describe 'DELETE /tasks/:id' do
-    it "renders success status" do
-      task = create(:task)
+    it 'renders success status' do
+      task = Task.create(content: 'Task Example')
 
       delete :destroy, params: { id: task.id }
 
       expect(Task.count).to eq(0)
-      expect(response.body).to eq({ success: true, status: 'Success' }.to_json)
+      expect(response.body).to eq({ success: true }.to_json)
     end
   end
 
   describe 'PUT /tasks/:id/mark_complete' do
-    it "renders modified task" do
-      task = create(:task)
+    it 'renders modified task' do
+      task = Task.create(content: 'Task Example')
 
-      put :mark_complete, params: { id: task.id }, format: :json
+      put :mark_complete, params: { id: task.id }
 
       expect(Task.where(completed: true).count).to eq(1)
 
       task.reload
-      expected_response = {
+      expect(response.body).to eq({
         task: {
           id: task.id,
           content: task.content,
@@ -80,22 +79,20 @@ RSpec.describe TasksController, type: :controller do
           created_at: task.created_at,
           updated_at: task.updated_at
         }
-      }
-
-      expect(response.body).to eq(expected_response.to_json)
+      }.to_json)
     end
   end
 
   describe 'PUT /tasks/:id/mark_active' do
-    it "renders modified task" do
-      task = create(:task, completed: true)
+    it 'renders modified task' do
+      task = Task.create(content: 'Task Example', completed: true)
 
-      put :mark_active, params: { id: task.id }, format: :json
+      put :mark_active, params: { id: task.id }
 
       expect(Task.where(completed: false).count).to eq(1)
 
       task.reload
-      expected_response = {
+      expect(response.body).to eq({
         task: {
           id: task.id,
           content: task.content,
@@ -103,9 +100,7 @@ RSpec.describe TasksController, type: :controller do
           created_at: task.created_at,
           updated_at: task.updated_at
         }
-      }
-
-      expect(response.body).to eq(expected_response.to_json)
+      }.to_json)
     end
   end
 end
